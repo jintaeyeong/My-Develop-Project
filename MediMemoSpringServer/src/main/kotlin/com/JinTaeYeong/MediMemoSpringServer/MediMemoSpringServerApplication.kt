03c376dd093989198ff82b0c04095457c5d8dfd0
진태영 <jjhtkfkd12@naver.com>
2024-08-28 21:40:39 +0900

    Spring Boot 서버 프로젝트 추가
    kotlin 환경에서 설정..
    Dependencies에 대해서는 설정한게 없음.. 좀 더 찾아보자

---
+++ MediMemoSpringServer/src/main/kotlin/com/JinTaeYeong/MediMemoSpringServer/MediMemoSpringServerApplication.kt
@@ -0,0 +1,11 @@
+package com.JinTaeYeong.MediMemoSpringServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MediMemoSpringServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MediMemoSpringServerApplication>(*args)
+}
---
+++ MediMemoSpringServer/src/test/kotlin/com/JinTaeYeong/MediMemoSpringServer/MediMemoSpringServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.JinTaeYeong.MediMemoSpringServer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MediMemoSpringServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
