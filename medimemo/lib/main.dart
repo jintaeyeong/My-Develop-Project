21c3b6c7a806b5712fd5c946c9a7fa57a6ed56ae
진태영 <jjhtkfkd12@naver.com>
2024-10-04 07:07:43 +0900

    Update main.dart

--- medimemo/lib/main.dart
+++ medimemo/lib/main.dart
@@ -11,12 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'MediMemo',
+      home: const MyHomePage(title: 'Medimemo 약물 기록 앱'),
     );
   }
 }
